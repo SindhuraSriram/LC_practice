60eb6315fc57150380535b9fe8758dbf0990d82c
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-01-25 11:11:58 -0500

    Time: 3 ms (90.21%), Space: 19.7 MB (6.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,14 +1,16 @@
 class Solution(object):
-    def findCircleNum(self, M):
-        seen = set([])
-        res = 0
-        for i in range(len(M)):
+    def findCircleNum(self, A):
+        N = len(A)
+        seen = set()
+        def dfs(node):
+            for nei, adj in enumerate(A[node]):
+                if adj and nei not in seen:
+                    seen.add(nei)
+                    dfs(nei)
+        
+        ans = 0
+        for i in range(N):
             if i not in seen:
-                toSee = [i]
-                while len(toSee):
-                    cur = toSee.pop()
-                    if cur not in seen:
-                        seen.add(cur)
-                        toSee = [j for j,v in enumerate(M[cur]) if v and j not in seen] + toSee
-                res += 1
-        return res      
+                dfs(i)
+                ans += 1
+        return ans   
