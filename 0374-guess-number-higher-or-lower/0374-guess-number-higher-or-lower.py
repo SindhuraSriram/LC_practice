459a062abbe61802e6e5b0b246d2f1d53bfa2d14
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-01-29 22:29:07 -0500

    Time: 33 ms (79.38%), Space: 17.6 MB (71.62%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -6,5 +6,17 @@
 # def guess(num: int) -> int:
 
 class Solution:
-    def guessNumber(self, n):
-        return bisect_left(range(n), 0, key=lambda m: -guess(m))
+    def guessNumber(self, n: int) -> int:
+        lowerBound, upperBound = 1, n
+        # Binary division faster than (lowerBound + upperBound) //2
+        myGuess = (lowerBound+upperBound) >> 1
+        # walrus operator ':=' - assigns value of the function to the variable 'res'
+        # and then compare res with 0
+        while (res := guess(myGuess)) != 0:
+            if res == 1:
+                lowerBound = myGuess+1
+            else:
+                upperBound = myGuess-1
+            myGuess = (lowerBound+upperBound) >> 1
+
+        return myGuess
