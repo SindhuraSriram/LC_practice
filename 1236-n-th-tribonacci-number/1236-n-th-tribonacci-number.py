026da56c39f2d1202959e206decd7757c73f15d9
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-01-31 16:15:18 -0500

    Time: 0 ms (100%), Space: 17.7 MB (61.01%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,18 +1,16 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = {}
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
         
-        def helper(n):
-            if n in memo:
-                return memo[n]
-            if n == 0:
-                return 0
-            elif n == 1 or n == 2:
-                return 1
-            else:
-                result = helper(n - 1) + helper(n - 2) + helper(n - 3)
-                memo[n] = result
-                return result
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 1
         
-        return helper(n)
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        
+        return dp[n]
         
