baf1512b9e2394c4b0435eaf3b6ac29eb6f5e1c8
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-02-02 14:50:08 -0500

    Time: 91 ms (89.77%), Space: 23.3 MB (88.51%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,22 +1,10 @@
-dp = []
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        dp = defaultdict(lambda : -1)
-        def cost(i,n,bought):
-            if i >= n:
-                return 0
-            if (i, bought) in dp:
-                return dp[(i, bought)]
-            #if bought previously, we have 2 option:-
-            # sell the prvious stock today or skip this day
-            if bought:
-                dp[(i,bought)] = max(cost(i+1,n,False) + prices[i] - fee, cost(i+1,n,bought))
-            
-            #else if we can buy a new stock, then we again have 2 option:-
-            #buy the current stock or skip over it
-            else:
-                dp[(i,bought)] = max(cost(i+1,n,True) - prices[i], cost(i+1,n,bought))
-                
-            return dp[(i,bought)]
-        
-        return cost(0,len(prices),False)        
+        buy = float('-inf')
+        sell = 0
+
+        for price in prices:
+            buy = max(buy, sell - price)
+            sell = max(sell, buy + price - fee)
+
+        return sell
