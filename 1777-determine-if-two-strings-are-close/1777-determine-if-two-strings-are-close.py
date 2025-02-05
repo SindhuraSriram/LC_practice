4ee74fed25e0ddafbcb2126b948da5bc0b796b2f
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-02-05 10:31:59 -0500

    Time: 75 ms (94.27%), Space: 18.3 MB (17.79%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,24 +1,10 @@
+from collections import Counter
 
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        freq1 = [0] * 26
-        freq2 = [0] * 26
+        # If the sets of unique characters in word1 and word2 are not equal, return False
+        if set(word1) != set(word2):
+            return False
 
-        for ch in word1:
-            freq1[ord(ch) - ord('a')] += 1
-
-        for ch in word2:
-            freq2[ord(ch) - ord('a')] += 1
-
-        for i in range(26):
-            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
-                return False
-
-        freq1.sort()
-        freq2.sort()
-
-        for i in range(26):
-            if freq1[i] != freq2[i]:
-                return False
-
-        return True        
+        # Compare sorted frequency counts
+        return sorted(Counter(word1).values()) == sorted(Counter(word2).values())
