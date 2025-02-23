d93b39333a6afbc6cfa03fff0dd4b309e13deeeb
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2025-02-23 17:11:24 -0500

    Time: 4 ms (94.29%), Space: 17.9 MB (82.38%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,7 @@
+#Python3 Code
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for letter in list(set(ransomNote)):
+            if ransomNote.count(letter) > magazine.count(letter): return False
+        return True
+        
